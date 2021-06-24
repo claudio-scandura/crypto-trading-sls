b14862f690574ad30997550729cc15dd9efe9e21
Claudio Scandura <cl.scandura@gmail.com>
2021-06-24 12:40:52 +0200

    Implement simple trading strategy.

--- crypto-trading/src/main/java/com/akkasls/hackathon/views/TradersByBaseAssetView.java
+++ crypto-trading/src/main/java/com/akkasls/hackathon/views/TradersByBaseAssetView.java
@@ -3,11 +3,15 @@ package com.akkasls.hackathon.views;
 import com.akkaserverless.javasdk.view.UpdateHandler;
 import com.akkaserverless.javasdk.view.View;
 import com.akkasls.hackathon.MovingAverageUpdated;
-import com.akkasls.hackathon.TraderState;
+import com.akkasls.hackathon.OrderPlaced;
 import com.akkasls.hackathon.TraderAdded;
+import com.akkasls.hackathon.TraderState;
+import com.akkasls.hackathon.entities.TraderEntity;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
 
+@Slf4j
 @View
 public class TradersByBaseAssetView {
 
@@ -22,5 +26,18 @@ public class TradersByBaseAssetView {
         return isLongMa
                 ? state.toBuilder().setLongMaValue(event.getValue()).build()
                 : state.toBuilder().setShortMaValue(event.getValue()).build();
+    }
+
+    @UpdateHandler
+    public TraderState processOrderPlaced(OrderPlaced event, TraderState state) {
+        switch (event.getType()) {
+            case "BUY":
+                return TraderEntity.buy(state, event.getQuantity(), event.getExchangeRate());
+            case "SELL":
+                return TraderEntity.sell(state, event.getQuantity(), event.getExchangeRate());
+            default:
+                log.warn("Unknown order type '{}'", event.getType());
+                return state;
+        }
     }
 }
