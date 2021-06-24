9821009837d124557bb5445a908b37dc027d0f07
manuelcueto <manuel.cueto@lightbend.com>
2021-06-24 16:39:54 +0100

    add view

--- crypto-trading/src/main/java/com/akkasls/hackathon/CryptoTradingServiceRunner.java
+++ crypto-trading/src/main/java/com/akkasls/hackathon/CryptoTradingServiceRunner.java
@@ -2,6 +2,7 @@ package com.akkasls.hackathon;
 
 import com.akkaserverless.javasdk.AkkaServerless;
 import com.akkasls.hackathon.entities.TraderEntity;
+import com.akkasls.hackathon.views.BalanceByAssetPairView;
 import com.akkasls.hackathon.views.TradersByBaseAssetView;
 import lombok.SneakyThrows;
 
@@ -19,6 +20,11 @@ public class CryptoTradingServiceRunner {
                         TradersByBaseAssetView.class,
                         Trading.getDescriptor().findServiceByName("TradersView"),
                         "tradersByBaseAsset",
+                        Trading.getDescriptor())
+                .registerView(
+                        BalanceByAssetPairView.class,
+                        Trading.getDescriptor().findServiceByName("BalanceView"),
+                        "balanceByAssetPair",
                         Trading.getDescriptor())
                 .start().toCompletableFuture().get();
     }
---
+++ crypto-trading/src/main/java/com/akkasls/hackathon/views/BalanceByAssetPairView.java
@@ -0,0 +1,43 @@
+package com.akkasls.hackathon.views;
+
+import com.akkaserverless.javasdk.view.UpdateHandler;
+import com.akkaserverless.javasdk.view.View;
+import com.akkasls.hackathon.MovingAverageUpdated;
+import com.akkasls.hackathon.OrderPlaced;
+import com.akkasls.hackathon.TraderAdded;
+import com.akkasls.hackathon.TraderState;
+import com.akkasls.hackathon.entities.TraderEntity;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Optional;
+
+@Slf4j
+@View
+public class BalanceByAssetPairView {
+
+    @UpdateHandler
+    public TraderState processTraderAdded(TraderAdded event, Optional<TraderState> maybeState) {
+        return maybeState.orElse(event.getTrader());
+    }
+
+    @UpdateHandler
+    public TraderState processMovingAverageUpdated(MovingAverageUpdated event, TraderState state) {
+        var isLongMa = event.getPeriod() == state.getLongMaPeriod();
+        return isLongMa
+                ? state.toBuilder().setLongMaValue(event.getValue()).build()
+                : state.toBuilder().setShortMaValue(event.getValue()).build();
+    }
+
+    @UpdateHandler
+    public TraderState processOrderPlaced(OrderPlaced event, TraderState state) {
+        switch (event.getType()) {
+            case "BUY":
+                return TraderEntity.buy(state, event.getQuantity(), event.getExchangeRate());
+            case "SELL":
+                return TraderEntity.sell(state, event.getQuantity(), event.getExchangeRate());
+            default:
+                log.warn("Unknown order type '{}'", event.getType());
+                return state;
+        }
+    }
+}
